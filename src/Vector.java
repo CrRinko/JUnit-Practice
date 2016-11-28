b17ea1ce06d17d9e3c4b7d7256ceaf04784c0eda
James <caterpillarous@yahoo.com>
2016-11-28 10:25:28 +0800

    framework

---
+++ src/Vector.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.List;
+
+/*************************************************************************
+ > File Name: Vector.Java
+ > Author: james
+ > Mail: caterpillarous@gmail.com
+ > Created Time: 2016/11/28 10:20
+ ************************************************************************/
+public class Vector {
+    private List<Vector> vectors;
+
+    private double getSimilarity(double v1, double v2){
+
+    }
+    public double getMostClose(Vector v, HashSet<Vector> vectorSet){
+
+    }
+}
